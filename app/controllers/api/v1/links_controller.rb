ddaed876b6bc51bc4bdefd0efae985b377fada74
SusiIrwin <susi.irwin@gmail.com>
2017-01-03 21:35:07 -0700

    WIP - Set up API

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::LinksController < ApplicationController
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      render json: @link, status: 201
+    else
+      render json: @link.errors.full_messages, status: 500
+    end
+  end
+
+  private
+  def link_params
+    params.permit(:url, :read_date)
+  end
+end
