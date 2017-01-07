a88978c8da9f6107dd8314832c7e41ccdfdd1fd1
SusiIrwin <susi.irwin@gmail.com>
2017-01-07 12:16:18 -0700

    Add local host to receive

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module Hotreads
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'localhost:3000',
+    'Access-Control-Request-Method' => %w{POST}
   end
 end
