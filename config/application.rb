b0c77598b4ba0dca44574c40090b86190f7ab18b
SusiIrwin <susi.irwin@gmail.com>
2017-01-07 13:28:50 -0700

    Add local host to receive

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Hotreads
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'https://blooming-bastion-32037.herokuapp.com',
+    'Access-Control-Allow-Origin' => 'http://blooming-bastion-32037.herokuapp.com',
     'Access-Control-Request-Method' => %w{POST}
     }
   end
